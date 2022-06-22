92f2f3f85c7d3fb9120e044c4adef896c704d12e
akash.ramani <akash.ramani@robinhood.com>
2022-06-22 11:19:24 -0700

    remove the redundant theme

--- app/src/main/java/awesome/shizzle/clayground/ui/MainActivity.kt
+++ app/src/main/java/awesome/shizzle/clayground/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package awesome.shizzle.clayground.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import awesome.shizzle.clayground.ui.ui.theme.ClaygroundTheme
+import awesome.shizzle.clayground.ui.theme.ClaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
